cd2b632f1473290138f72593d5cb4d2faf2c0d3f
Abdullah Al-Tamimi <abdulla20103@gmail.com>
2016-12-17 22:18:25 +0300

    Cryptography: Updating the iv for the encryption and decryption

--- app/src/main/java/com/amanapp/crypto/Cryptography.java
+++ app/src/main/java/com/amanapp/crypto/Cryptography.java
@@ -8,6 +8,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 
@@ -15,14 +16,17 @@ import javax.crypto.Cipher;
 import javax.crypto.CipherInputStream;
 import javax.crypto.CipherOutputStream;
 import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 
 class Cryptography {
 
-    static File encrypt(Cipher cipher, File file) throws IOException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException {
+    static File encrypt(Cipher cipher, File file, byte[] iv) throws IOException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException {
         Log.d(UploadFileTask.TAG, "Cryptography encrypt - File:" + file.getAbsoluteFile() + ", cipher is null? " + (cipher == null));
         File outFile = new File(file.getAbsolutePath() + ".aman");
         FileInputStream fis = new FileInputStream(file);
         FileOutputStream fos = new FileOutputStream(outFile);
+        fos.write(iv);
         CipherOutputStream cos = new CipherOutputStream(fos, cipher);
         int b;
         byte[] d = new byte[8];
@@ -36,10 +40,13 @@ class Cryptography {
         return outFile;
     }
 
-    static File decrypt(Cipher cipher, File file) throws IOException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException {
+    static File decrypt(Cipher cipher, File file, SecretKeySpec key) throws IOException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException {
         File outFile = new File(file.getAbsolutePath().replace(".aman", ""));
         FileInputStream fis = new FileInputStream(file);
         FileOutputStream fos = new FileOutputStream(outFile);
+        byte[] iv = new byte[16];
+        fis.read(iv);
+        cipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(iv));
         CipherInputStream cis = new CipherInputStream(fis, cipher);
         int b;
         byte[] d = new byte[8];
--- app/src/main/java/com/amanapp/crypto/FileCrypto.java
+++ app/src/main/java/com/amanapp/crypto/FileCrypto.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 
 import javax.crypto.Cipher;
 import javax.crypto.NoSuchPaddingException;
@@ -20,26 +21,31 @@ import javax.crypto.spec.IvParameterSpec;
 
 public class FileCrypto {
 
-    private static final byte[] ivBytes = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
     private Cipher cipher;
-    private IvParameterSpec ivSpec;
 
     public FileCrypto() throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException {
         Log.d(UploadFileTask.TAG, "File Crypto Constructor");
         cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        ivSpec = new IvParameterSpec(ivBytes);
         Log.d(UploadFileTask.TAG, "File Crypto constructed");
     }
 
     public File encrypt(File file) throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, IOException, InvalidAlgorithmParameterException {
         Log.d(UploadFileTask.TAG, "File Crypto encrypt - File:" + file.getAbsoluteFile());
-        cipher.init(Cipher.ENCRYPT_MODE, SecretKey.get(), ivSpec);
-        return Cryptography.encrypt(cipher, file);
+        byte[] iv = getIvBytes();
+        cipher.init(Cipher.ENCRYPT_MODE, SecretKey.get(), new IvParameterSpec(iv));
+        return Cryptography.encrypt(cipher, file, iv);
     }
 
     public File decrypt(File file) throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, IOException, InvalidAlgorithmParameterException {
-        cipher.init(Cipher.DECRYPT_MODE, SecretKey.get(), ivSpec);
-        return Cryptography.decrypt(cipher, file);
+//        cipher.init(Cipher.DECRYPT_MODE, SecretKey.get(), ivSpec);
+        return Cryptography.decrypt(cipher, file, SecretKey.get());
+    }
+
+    private byte[] getIvBytes() throws NoSuchAlgorithmException {
+        byte[] ivBytes = new byte[16];
+        SecureRandom secureRandom = SecureRandom.getInstance("SHA1PRNG");
+        secureRandom.nextBytes(ivBytes);
+        return ivBytes;
     }
 
 }
