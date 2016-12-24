434a7d8a3bbe4f5fa1df6c83d2f04a2ba49cdc31
Abdullah Al-Tamimi <abdulla20103@gmail.com>
2016-12-24 12:57:26 +0300

    Fix #1

--- app/src/main/java/com/amanapp/dropbox/DropboxClient.java
+++ app/src/main/java/com/amanapp/dropbox/DropboxClient.java
@@ -5,20 +5,21 @@ import com.dropbox.core.http.OkHttp3Requestor;
 import com.dropbox.core.v2.DbxClientV2;
 
 /**
- * Singleton instance of {@link DbxClientV2} and friends
+ * Singleton instance of {@link DbxClientV2}
  */
 public class DropboxClient {
 
     private static DbxClientV2 sDbxClient;
 
+    private DropboxClient() {
+    }
+
     public static void init(String accessToken) {
-        if (sDbxClient == null) {
             DbxRequestConfig requestConfig = DbxRequestConfig.newBuilder("Aman/0.0.1")
                     .withHttpRequestor(OkHttp3Requestor.INSTANCE)
                     .build();
 
             sDbxClient = new DbxClientV2(requestConfig, accessToken);
-        }
     }
 
     public static DbxClientV2 getClient() {
